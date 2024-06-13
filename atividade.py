2b254f3e5f9531ca76edade5ba53c05e4816e05a
Isadora Silva <silvairs9292@gmail.com>
2024-06-13 20:36:14 -0300

    First Commit

---
+++ atividade.py
@@ -0,0 +1,28 @@
+nome_completo = "Paula"
+valor = "500"
+mes = "Janeiro"
+desconto = 10
+cupom = nome , "É", desconto
+
+
+
+print ("Paula")
+print ("500")
+print ("Janeiro")
+print ("10")
+print ("PAULAÉ10")
+print ("Paula" + nome + "teve a primeira nota" + n1, + n2,)
+
+
+"""
+Olá, PAULA MARTINS. Em JANEIRO você realizou uma compra no valor de R$500,00 e ganhou um desconto de 10% em sua próxima compra. Use o cupom PAULAÉ10.
+"""
+nome_completo = "PAULA MARTINS"
+nome = "PAULA"
+mês = "JANEIRO"
+valor = "R$ 500,00"
+desconto = 10
+cupom = nome+"É"+str(desconto)
+
+
+print("Olá, " + nome_completo , ". Em JANEIRO você realizou uma compra no valor de", valor , "e ganhou um desconto de", desconto ,"% em sua próxima compra. Use o cupom" , cupom )
---
+++ aula1.py
@@ -0,0 +1,9 @@
+print("Isadora")
+print ("DESAFIO DO CAÍQUE")
+print(145 + 234)
+
+
+nome = "Isadora"
+idade = 25
+saldo = 0.5
+e_calvo = True
